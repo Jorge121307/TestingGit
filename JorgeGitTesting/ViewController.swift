c1917e62bebbed718b69f3e19531cc689987d52f
Jorge Arechiga <jorgearechiga@Jorges-MacBook-Pro.local>
2020-01-10 08:55:57 -0700

    First Commit

---
+++ JorgeGitTesting/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JorgeGitTesting
+//
+//  Created by Jorge Arechiga on 1/10/20.
+//  Copyright © 2020 Jorge Arechiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Look I made a change!
+        
+    }
+
+
+}
+
